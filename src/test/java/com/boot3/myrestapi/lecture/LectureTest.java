2c9bd1ce128871143c86f83634355c99a5867e4e
donghass <ehdgk1221@gmail.com>
2025-02-13 11:23:55 +0900

    start

---
+++ src/test/java/com/boot3/myrestapi/lecture/LectureTest.java
@@ -0,0 +1,33 @@
+package com.boot3.myrestapi.lecture;
+
+import com.boot3.myrestapi.lectures.Lecture;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat
+;
+import static org.junit.jupiter.api.Assertions.*;
+public class LectureTest
+{
+@Test
+public void builder() {
+    Lecture lecture = Lecture.builder()
+        .name("Spring REST API")
+        .description("REST API developmemt with Spring")
+        .build();
+    // 테스트 값을 체크
+    assertEquals("Spring REST API", lecture.getName());
+}
+@Test
+public void javaBean()
+    {
+        String name = "Lecture"; //Give
+        String description = "Spring";
+        Lecture lecture = new Lecture(); //When
+        lecture.setName(name);
+        lecture.setDescription(description);
+        assertThat
+            (lecture.getName()).isEqualTo("Lecture"); //Then
+        assertThat
+            (lecture.getDescription()).isEqualTo("Spring");
+    }
+}
