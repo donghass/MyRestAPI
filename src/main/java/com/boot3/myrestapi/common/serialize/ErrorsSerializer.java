367ac56257919b9e0e84425c584ec83739ba68c2
donghass <ehdgk1221@gmail.com>
2025-02-13 16:58:41 +0900

    LambdaTest add

--- src/main/java/com/boot3/myrestapi/common/serialize/ErrorsSerializer.java
+++ src/main/java/com/boot3/myrestapi/common/serialize/ErrorsSerializer.java
@@ -13,17 +13,17 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 public class ErrorsSerializer extends JsonSerializer<Errors>{
     @Override
     public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        gen.writeStartArray();
-        errors.getFieldErrors().forEach(e -> {
+        gen.writeStartArray(); // 직렬화 시작
+        errors.getFieldErrors().forEach(e -> {      // FieldErrors 객체 정보 직렬화
             try {
                 gen.writeStartObject();
-                gen.writeStringField("field", e.getField());
-                gen.writeStringField("objectName", e.getObjectName());
-                gen.writeStringField("code", e.getCode());
-                gen.writeStringField("defaultMessage", e.getDefaultMessage());
+                gen.writeStringField("field", e.getField());                            //필드명
+                gen.writeStringField("objectName", e.getObjectName());                  //필드가 포함된 DTO 객체명
+                gen.writeStringField("code", e.getCode());                              //검증규칙(어노테이션)
+                gen.writeStringField("defaultMessage", e.getDefaultMessage());          //에러메세지
                 Object rejectedValue = e.getRejectedValue();
                 if (rejectedValue != null) {
-                    gen.writeStringField("rejectedValue", rejectedValue.toString());
+                    gen.writeStringField("rejectedValue", rejectedValue.toString());    //잘못 입력된 값
                 }
                 gen.writeEndObject();
             } catch (IOException e1) {
@@ -31,7 +31,7 @@ public class ErrorsSerializer extends JsonSerializer<Errors>{
             }
         });
 
-        errors.getGlobalErrors().forEach(e -> {
+        errors.getGlobalErrors().forEach(e -> {     // (ObjectError) 두개 이상의 필드에 걸쳐 에러를 확인해야할때 사용
             try {
                 gen.writeStartObject();
                 gen.writeStringField("objectName", e.getObjectName());
@@ -42,7 +42,7 @@ public class ErrorsSerializer extends JsonSerializer<Errors>{
                 e1.printStackTrace();
             }
         });
-        gen.writeEndArray();
+        gen.writeEndArray(); // 직렬화 끝
 
     }
 }
