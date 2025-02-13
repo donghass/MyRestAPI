6d58527fb32129ed32d8d11c8faffbd75388eb49
donghass <ehdgk1221@gmail.com>
2025-02-13 13:52:08 +0900

    Entitiy add

--- src/main/java/com/boot3/myrestapi/lectures/Lecture.java
+++ src/main/java/com/boot3/myrestapi/lectures/Lecture.java
@@ -1,6 +1,7 @@
 package com.boot3.myrestapi.lectures;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,9 +11,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter @Setter
 @EqualsAndHashCode(of="id")
+@Entity
+@Table(name="lectures")
 public class Lecture {
-    private Integer id
-    ;
+    // pk / 값 자동 상승(mysql 계열 autoincrement) 1씩 자동 상승 // 오라클은 sequence
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    // not null
+    @Column(nullable = false)
     private String name;
     private String description;
     @JsonFormat(pattern="yyyy-MM-dd HH:mm")
@@ -26,9 +32,10 @@ public class Lecture {
     private String location;
     private int basePrice;
     private int maxPrice;
-    private int limitOfEnrollment
-    ;
+    private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    // 숫자or문자열 설정 // string는 문자열 설정
+    @Enumerated(EnumType.STRING)
     private LectureStatus lectureStatus = LectureStatus.DRAFT;
 }
