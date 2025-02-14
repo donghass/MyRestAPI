af1943c83d471cbda4c034045ab6bade928009d6
donghass <ehdgk1221@gmail.com>
2025-02-14 12:53:03 +0900

    BusinessException add

--- src/main/java/com/boot3/myrestapi/lectures/LectureController.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -1,6 +1,7 @@
 package com.boot3.myrestapi.lectures;
 
 import com.boot3.myrestapi.common.ErrorsResource;
+import com.boot3.myrestapi.common.exception.BusinessException;
 import com.boot3.myrestapi.lectures.dto.LectureReqDto;
 import com.boot3.myrestapi.lectures.dto.LectureResDto;
 import com.boot3.myrestapi.lectures.dto.LectureResource;
@@ -14,6 +15,7 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -121,4 +123,24 @@ public class LectureController {
         PagedModel<LectureResource> pagedModel = assembler.toModel(lectureResDtoPage, LectureResource::new);
         return ResponseEntity.ok(pagedModel);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getLecture(@PathVariable Integer id) {
+        Optional<Lecture> optionalLecture = this.lectureRepository.findById(id);    // Optional 은 반환받은 데이터의 null 체크를 수동으로 하지 않아도됨
+
+//        if(optionalLecture.isEmpty()) {
+////            return ResponseEntity.notFound().build();
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("강의가 없습니다.");
+//        }
+//        Lecture lecture = optionalLecture.get();
+
+        String errMsg = String.format("Id = %d 강의가 없습니다.",id);
+        // 위에 네줄 생략 - Optional 의 저장된 객체가 null 이면 오류 반환 null 이 아니면 객체 반환
+        Lecture lecture = optionalLecture.orElseThrow(()->new BusinessException(errMsg, HttpStatus.NOT_FOUND));
+
+        LectureResDto lectureResDto = modelMapper.map(lecture, LectureResDto.class);
+        LectureResource lectureResource = new LectureResource(lectureResDto);
+        return ResponseEntity.ok(lectureResource);
+    }
+
 }
